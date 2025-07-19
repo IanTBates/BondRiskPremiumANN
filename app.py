a80ff75d8b68404f62eac08917e45724d0bb85de
IanTBates <itb4286@utulsa.edu>
2025-07-19 11:49:48 -0500

    added some file upload code

--- app.py
+++ app.py
@@ -10,27 +10,24 @@ from shiny import App, ui
 
 from yielddashboard import yielddashboard as yield_db
 
-yield_db = yield_db()
-
-app_ui = ui.page_navbar(  
-    ui.nav_panel("Yield Analyis", yield_db.page()),  
-    ui.nav_panel("Data Analysis", "Page B content"),  
-    ui.nav_panel("Artificial Neural Network", "Page C content"),  
-    title="Bond Risk Premium Analysis App",  
-    id="page",  
-)  
-
-
-
-
-def server(input, output, session):
-    pass
-
-
-app = App(app_ui, server)
-
 
 if __name__ == "__main__":
-    pass
+    yield_db = yield_db()
+
+    app_ui = ui.page_navbar(  
+        ui.nav_panel("Yield Analyis", yield_db.page()),  
+        ui.nav_panel("Data Analysis", "Data Analysis content"),  
+        ui.nav_panel("Artificial Neural Network", "Artificial Neural Network content"),  
+        title="Bond Risk Premium Analysis App",  
+        id="page",  
+    )  
+
+
+
+
+    def server(input, output, session):
+        pass
+
+    app = App(app_ui, server)
     
 
