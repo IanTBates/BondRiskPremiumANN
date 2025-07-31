851c47d9a7beea1a7fc157686b760f2ddc97f27f
IanTBates <itb4286@utulsa.edu>
2025-07-31 00:06:08 -0500

    update

--- app.py
+++ app.py
@@ -11,23 +11,22 @@ from shiny import App, ui
 from yielddashboard import yielddashboard as yield_db
 
 
-if __name__ == "__main__":
-    yield_db = yield_db()
+yield_db = yield_db()
 
-    app_ui = ui.page_navbar(  
-        ui.nav_panel("Yield Analyis", yield_db.page()),  
-        ui.nav_panel("Data Analysis", "Data Analysis content"),  
-        ui.nav_panel("Artificial Neural Network", "Artificial Neural Network content"),  
-        title="Bond Risk Premium Analysis App",  
-        id="page",  
-    )  
-
+app_ui = ui.page_navbar(  
+    ui.nav_panel("Yield Analyis", yield_db.page()),  
+    ui.nav_panel("Data Analysis", "Data Analysis content"),  
+    ui.nav_panel("Artificial Neural Network", "Artificial Neural Network content"),  
+    title="Bond Risk Premium Analysis App",  
+    id="page",  
+)  
 
 
 
-    def server(input, output, session):
-        pass
 
-    app = App(app_ui, server)
+def server(input, output, session):
+    pass
+
+app = App(app_ui, server)
     
 
